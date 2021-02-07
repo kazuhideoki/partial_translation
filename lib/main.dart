7a3b03e44899594b5a516f3f5962084b312d3c36
Kazuhide Oki <cedar123pc@gmail.com>
2021-02-08 07:22:15 +0900

    flutter_hooksに変更

--- lib/main.dart
+++ lib/main.dart
@@ -1,112 +1,94 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(new MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => new _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-
+class MyApp extends HookWidget {
   InAppWebViewController webView;
-  String url = "";
-  double progress = 0;
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
+    // final useState
+    final url = useState('');
+    final progress = useState(0.1 as double); // 0でうまく出来なかった
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text(
-                  "CURRENT URL\n${(url.length > 50) ? url.substring(0, 50) + "..." : url}"),
+              "CURRENT URL\n${url.value}"),
         ),
         body: Container(
-          child: Column(children: <Widget>[
-            Container(
-                child: progress < 1.0
-                    ? LinearProgressIndicator(value: progress)
-                    : Container()),
-            Expanded(
-              child: Container(
-                decoration:
-                    BoxDecoration(border: Border.all(color: Colors.blueAccent)),
-                child: InAppWebView(
-                  initialUrl: "https://flutter.dev/",
-                  initialHeaders: {},
-                  initialOptions: InAppWebViewGroupOptions(
+            child: Column(children: <Widget>[
+          Container(
+              child: progress.value < 1.0
+                  ? LinearProgressIndicator(value: progress.value)
+                  : Container()),
+          Expanded(
+            child: Container(
+              decoration:
+                  BoxDecoration(border: Border.all(color: Colors.blueAccent)),
+              child: InAppWebView(
+                initialUrl: "https://flutter.dev/",
+                initialHeaders: {},
+                initialOptions: InAppWebViewGroupOptions(
                     crossPlatform: InAppWebViewOptions(
-                        debuggingEnabled: true,
-                    )
-                  ),
-                  onWebViewCreated: (InAppWebViewController controller) {
-                    webView = controller;
-                  },
-                  onLoadStart: (InAppWebViewController controller, String url) {
-                    setState(() {
-                      this.url = url;
-                    });
-                  },
-                  onLoadStop: (InAppWebViewController controller, String url) async {
-                    setState(() {
-                      this.url = url;
-                    });
-                  },
-                  onProgressChanged: (InAppWebViewController controller, int progress) {
-                    setState(() {
-                      this.progress = progress / 100;
-                    });
-                  },
-                ),
+                  debuggingEnabled: true,
+                )),
+                onWebViewCreated: (InAppWebViewController controller) {
+                  webView = controller;
+                },
+                onLoadStart:
+                    (InAppWebViewController controller, String newUrl) {
+                  url.value = newUrl;
+                },
+                onLoadStop:
+                    (InAppWebViewController controller, String newUrl) async {
+                  url.value = newUrl;
+                },
+                onProgressChanged:
+                    (InAppWebViewController controller, int newProgress) {
+                    progress.value = newProgress / 100;
+                },
               ),
             ),
-            ButtonBar(
-              alignment: MainAxisAlignment.center,
-              children: <Widget>[
-                RaisedButton(
-                  child: Icon(Icons.arrow_back),
-                  onPressed: () {
-                    if (webView != null) {
-                      webView.goBack();
-                    }
-                  },
-                ),
-                RaisedButton(
-                  child: Icon(Icons.arrow_forward),
-                  onPressed: () {
-                    if (webView != null) {
-                      webView.goForward();
-                    }
-                  },
-                ),
-                RaisedButton(
-                  child: Icon(Icons.refresh),
-                  onPressed: () {
-                    if (webView != null) {
-                      webView.reload();
-                    }
-                  },
-                ),
-              ],
-            ),
+          ),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                child: Icon(Icons.arrow_back),
+                onPressed: () {
+                  if (webView != null) {
+                    webView.goBack();
+                  }
+                },
+              ),
+              RaisedButton(
+                child: Icon(Icons.arrow_forward),
+                onPressed: () {
+                  if (webView != null) {
+                    webView.goForward();
+                  }
+                },
+              ),
+              RaisedButton(
+                child: Icon(Icons.refresh),
+                onPressed: () {
+                  if (webView != null) {
+                    webView.reload();
+                  }
+                },
+              ),
+            ],
+          ),
         ])),
       ),
     );
   }
 }
-
