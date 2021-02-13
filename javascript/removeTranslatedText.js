1e0fc5312ba21c321e2895ca616dc85b95b464a9
Kazuhide Oki <cedar123pc@gmail.com>
2021-02-13 09:15:05 +0900

    翻訳結果の表示非表示切り替え

--- javascript/removeTranslatedText.js
+++ javascript/removeTranslatedText.js
@@ -1,6 +1,4 @@
-var targetElements = document.getElementsByClassName(
-  "modified-by-partial-translate"
-);
+var targetElements = document.getElementsByClassName("modified");
 console.log(targetElements);
 Array.prototype.forEach.call(targetElements, function (element) {
   console.log("削除したのは " + element.textContent);
--- javascript/replaceText.js
+++ javascript/replaceText.js
@@ -1,119 +1,45 @@
 console.log("★★★◆◆◆replaceText.js◆◆◆★★★");
-var tagRegExp = /<(".*?"|'.*?'|[^'"])*?>/;
-var temporallyRemovedNodes = [];
 
+// 選択部分を取得
 var range = document.getSelection().getRangeAt(0);
-var endContainer = range.endContainer;
+// 翻訳結果のnodeを作る
+var insertingNodes = createInsertingNodes();
+// 翻訳結果を挿入
+replaceNode(range, insertingNodes);
+
+console.log(range.endContainer.parentElement.innerHTML);
+
+function createInsertingNodes() {
+  var c = window.localStorage.getItem("count") || "0";
+  var ptData = window.localStorage.getItem(`ptData${c}`);
+  var originalText = JSON.parse(ptData).originalText;
+  var translatedText = JSON.parse(ptData).translatedText;
+
+  var originalPart = document.createElement("b");
+  originalPart.innerHTML = `<b id="original${c}" class="modified" style="color: blue;">${originalText}</b>`;
+  var translatedPart = document.createElement("b");
+  translatedPart.innerHTML = `<b id="translated${c}" class="modified" style="color: red;">${translatedText}</b>`;
+
+  var insertingNodes = document.createElement("span");
+  insertingNodes.appendChild(originalPart);
+  insertingNodes.appendChild(translatedPart);
+
+  // insertingNodes.onclick =
+  //   insertingNodes.style.display === "none" ? "inline" : "none";
+  insertingNodes.addEventListener("click", function () {
+    console.log(`◆◆◆${originalText} is Clicked!!!◆◆◆`);
+    if (translatedPart.style.display === "none") {
+      translatedPart.style.display = "inline";
+    } else {
+      translatedPart.style.display = "none";
+    }
+  });
+
+  return insertingNodes;
+}
 
 function replaceNode(range, node) {
   range.insertNode(node);
   range.setStartAfter(node);
   range.deleteContents();
 }
-
-function generateInsertingNodesString() {
-  var c = window.localStorage.getItem("count") || "0";
-  var ptData = window.localStorage.getItem(`ptData${c}`);
-  console.log("ptDataは " + ptData);
-  var originalText = JSON.parse(ptData).originalText;
-  console.log("originalTextは " + originalText);
-  var translatedText = JSON.parse(ptData).translatedText;
-
-  var originalPart = `<b id="original${c}" class="modified" style="color: blue;">${originalText}</b>`;
-  var translatedPart = `<b id="translated${c}" class="modified" style="color: red;">${translatedText}</b>`;
-
-  return originalPart + translatedPart;
-}
-
-var insertingNodes = document.createElement("span");
-insertingNodes.innerHTML = generateInsertingNodesString();
-replaceNode(range, insertingNodes);
-
-// var endContainerInnerHTML = range.endContainer.parentNode.innerHTML;
-// console.log("endContainerInnerHTMLは " + endContainerInnerHTML);
-
-// var offsetMin = Math.min(range.endOffset, range.startOffset);
-// // console.log("offsetMinは " + offsetMin);
-// var offsetMax = Math.max(range.endOffset, range.startOffset);
-// // console.log("offsetMaxは " + offsetMax);
-// var firstPart = endContainerInnerHTML.toString().substr(0, offsetMin);
-// console.log("最初のfirstPartは " + firstPart);
-
-// var lastPart = endContainerInnerHTML.substr(offsetMax);
-
-// // ◆◆◆substrしたあとのinnerHTMLからgetElementsByClassNameできるか？
-// var tempElm = document.createElement("div");
-// tempElm.innerHTML = endContainerInnerHTML;
-// console.log("tempElm.innerHTMLは " + tempElm.innerHTML);
-// var translatedNode = tempElm.getElementsByClassName(
-//   "modified"
-//   // ""
-// );
-// console.log(
-//   "endContainerInnerHTMLのtranslatedNodeは " + JSON.stringify(translatedNode)
-// );
-
-// function retrieveTranslatedNodes(node, offset) {
-//   console.log("◆◆◆retrieveTranslatedNodes◆◆◆");
-//   var parentNode = endContainer.parentNode.innerHTML;
-//   console.log("retrieveTranslatedNodesのparentNodeは " + parentNode);
-
-//   var firstPart = parentNode.substr(0, offset);
-//   var lastPart = parentNode.substr(offset + node.length);
-
-//   var newNodeString = firstPart + node + lastPart;
-
-//   parentNode.innerHTML = newNodeString;
-// }
-// function insertTranslatedNode() {
-//   console.log("◆◆◆insertTranslatedNode◆◆◆");
-//   // 選択の直後に翻訳元テキストと結果を挿入
-//   var originalPart = `<b id="original${c}" class="modified" style="color: blue;">${originalText}</b>`;
-//   var translatedPart = `<b id="translated${c}" class="modified" style="color: red;">${translatedText}</b>`;
-
-//   var newNodeString = firstPart + originalPart + translatedPart + lastPart;
-
-//   endContainer.parentNode.innerHTML = newNodeString;
-
-//   window.localStorage.setItem("count", String(Number(c) + 1));
-// }
-
-// function checkHasNodesBeforeSelection() {
-//   var result = translatedNode.length ? true : false;
-//   console.log("◆◆◆checkHasNodesBeforeSelection 返り値は " + result + " ◆◆◆");
-//   return result;
-// }
-
-// // 前半部分(firstPart)に翻訳済みNode(translatedNode)があるとreplaceがうまく行かないので一時的に削除
-// function removeNode() {
-//   console.log("◆◆◆removeNode◆◆◆");
-//   // offsetの取得, 正規表現で最初にタグにがあるところ
-//   // var offset = firstPart.search(tagRegExp);
-//   var offset = firstPart.search(/</);
-//   console.log("removeNodeのoffsetは" + offset);
-
-//   // 該当タグを一時的にlocalStorageに退避する
-//   var NodesClone = [...translatedNode];
-//   temporallyRemovedNodes.push({
-//     node: NodesClone[0],
-//     offset: offset,
-//   });
-
-//   // Nodeを削除する
-//   translatedNode[0].remove();
-// }
-
-// while (checkHasNodesBeforeSelection()) {
-//   removeNode();
-// }
-
-// insertTranslatedNode();
-// console.log(
-//   "temporallyRemovedNodesは " + JSON.stringify(temporallyRemovedNodes)
-// );
-
-// while (temporallyRemovedNodes.length) {
-//   var data = temporallyRemovedNodes.pop();
-
-//   retrieveTranslatedNodes(data.node, data.offset);
-// }
