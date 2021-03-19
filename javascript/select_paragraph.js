f8bdfedcfc705158c026c327e0fdfbbaf902b3c1
Kazuhide Oki <cedar123pc@gmail.com>
2021-03-20 07:22:41 +0900

    タップ時（moveしない）のみandroidで挙動確認, ios(実機も)ではうまく動作しない

--- javascript/select_paragraph.js
+++ javascript/select_paragraph.js
@@ -1,9 +1,32 @@
 console.log("★★★◆◆◆select_paragraph.js★★★◆◆◆");
 
+var isMoved = false;
+var targetEvent;
+
 // localStorage経由でisSelectParagraphを渡して, 発火するか否かを判定する。
-window.addEventListener("touchstart", selectParagraph, true);
+window.addEventListener(
+  "touchstart",
+  (e) => {
+    console.log("touchstart");
+    isMoved = false;
+    targetEvent = e;
+  },
+  true
+);
+
+window.addEventListener(
+  "touchmove",
+  () => {
+    console.log("touchmove");
+    isMoved = true;
+  },
+  true
+);
+
+window.addEventListener("touchend", selectParagraph, true);
 
 function selectParagraph(e) {
+  console.log("touchend");
   let isSelectParagraph = window.localStorage.getItem("isSelectParagraph");
   if (isSelectParagraph === "true") {
     isSelectParagraph = true;
@@ -11,9 +34,12 @@ function selectParagraph(e) {
     isSelectParagraph = false;
   }
 
-  if (isSelectParagraph) {
+  console.log(`isMovedは ${isMoved}`);
+
+  if (isSelectParagraph && isMoved === false) {
     try {
-      const touchedElement = e.targetTouches.item(0).target;
+      // const touchedElement = targetEvent.targetTouches.item(0).target; // android でのみ成功 iosでは一瞬選択される
+      const touchedElement = e.target; // android でのみ成功 iosでは一瞬選択される
       const range = new Range();
       range.selectNodeContents(touchedElement);
 
